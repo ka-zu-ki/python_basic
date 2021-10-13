0b399540df675302f59506b6da32e47acf81644a
ka-zu-ki <yamakazuki0721@gmail.com>
2021-10-13 16:52:45 +0900

    univ04

---
+++ univ/04.py
@@ -0,0 +1,16 @@
+for number in [1, 2, 3, 4, 5]:
+    print(number)
+
+for number in [1, 2, 3, 4, 5]:
+    print(number)
+    print("こんにちは")
+
+for number in [1, 2, 3, 4, 5]:
+    print(number)
+print("こんにちは")
+
+age = 21
+print("私は" + str(age) + "歳です")
+
+year = 2018
+print("令和元年は西暦" + str(year) + "年です")
