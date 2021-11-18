6b0a3ab79d1894bb8713137e06e7cf6bfbfe4e8b
ka-zu-ki <yamakazuki0721@gmail.com>
2021-11-18 13:57:40 +0900

    univ09

---
+++ univ/09.py
@@ -0,0 +1,10 @@
+def sumOfInt(x, y, z):
+    print(x + y + z)
+
+def sumOfInputInt():
+    x = int(input('数字を入力してください'))
+    y = int(input('数字を入力してください'))
+    print(x + y)
+
+sumOfInt(1, 2, 3)
+sumOfInputInt()
